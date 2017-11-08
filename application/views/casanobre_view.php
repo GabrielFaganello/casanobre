f6f0dd873fed9a84134aa1a5479e30d7b1358db1
Gabriel Faganello <faganello_@hotmail.com>
2017-11-08 01:00:30 -0200

    arrumando quebra

--- application/views/casanobre_view.php
+++ application/views/casanobre_view.php
@@ -25,37 +25,44 @@
 
             <div class="row">
                 <div class="col-md-6">
-                    <div class="col-md-1 col-md-offset-3 text-right"><i class="fa fa-quote-left fa-3x" style="color: #C24F56;"></i></div>
-                    <div class="col-md-8" style="font-family: Thin_Italic;"><p>Montar minha casa com os móveis da <br>Casa Nobre foi a melhor escolha.<br>Ótimo atendimento!<br><i class="fa fa-quote-right fa-2x" style="color: #F1BDC4; margin-left: 40%"></i></p></div>
+                    <div class="row">
+                        <div class="col-md-1 col-md-offset-3 text-right"><i class="fa fa-quote-left fa-3x" style="color: #C24F56;"></i></div>
+                        <div class="col-md-8" style="font-family: Thin_Italic;"><p>Montar minha casa com os móveis da <br>Casa Nobre foi a melhor escolha.<br>Ótimo atendimento!<br><i class="fa fa-quote-right fa-2x" style="color: #F1BDC4; margin-left: 40%"></i></p></div>
                     <!-- <div class="col-md-1 text-left"><i class="fa fa-quote-right fa-3x" style="color: #F1BDC4;"></div> -->
+                    </div>
+                    <div class="row">
+                        <div class="col-md-2 col-md-offset-3" style="width: 15%;">
+                            <img alt="image" class="img-circle" src="resources/img/test-1.jpg">
+                        </div>
+                        <div class="col-md-7 text-left pull-left">
+                            <p></p>
+                            <p style="font-family: Bold_Font; line-height: 18px;">Izabel Pacheco<br>
+                            <span style="font-family: Regular;">Fez cozinha e banheiro<br>
+                            com a <b style="font-family: Bold_Italic;">Casa Nobre Planejados<b></span></p>
+                        </div>                    
+                    </div>
                 </div>
                 <div class="col-md-6">
-                    <div class="col-md-1 col-md-offset-1 text-right"><i class="fa fa-quote-left fa-3x" style="color: #C24F56;"></i></div>
-                    <div class="col-md-8" style="font-family: Thin_Italic;"><p>Montar minha casa com os móveis da <br>Casa Nobre foi a melhor escolha.<br>Ótimo atendimento!<br><i class="fa fa-quote-right fa-2x" style="color: #F1BDC4; margin-left: 40%"></i></p></div>
-                    <!-- <div class="col-md-1 text-left"><i class="fa fa-quote-right fa-3x" style="color: #F1BDC4;"></div> -->
+                    <div class="row">
+                        <div class="col-md-1 col-md-offset-1 text-right"><i class="fa fa-quote-left fa-3x" style="color: #C24F56;"></i></div>
+                        <div class="col-md-8" style="font-family: Thin_Italic;"><p>Montar minha casa com os móveis da <br>Casa Nobre foi a melhor escolha.<br>Ótimo atendimento!<br><i class="fa fa-quote-right fa-2x" style="color: #F1BDC4; margin-left: 40%"></i></p></div>
+
+                    </div>
+                    <div class="row">
+                        <div class="col-md-2 col-md-offset-3" style="width: 15%; margin-left: 11%;">
+                            <img alt="image" class="img-circle" src="resources/img/test-1.jpg">
+                        </div>
+                        <div class="col-md-7 text-left pull-left">
+                            <p></p>
+                            <p style="font-family: Bold_Font; line-height: 18px;">Chaves<br>
+                            <span style="font-family: Regular;">Fez o baú<br>
+                            com a <b style="font-family: Bold_Italic;">Casa Nobre Planejados<b></span></p>
+                        </div>
+                    </div>
                 </div>
             </div>
 
-            <div class="row">
-                <div class="col-md-1 col-md-offset-1" style="width: 15%;">
-                    <img alt="image" class="pull-right img-cn img-circle" src="resources/img/test-1.jpg">
-                </div>
-                <div class="col-md-2 text-left pull-left">
-                    <p></p>
-                    <p style="font-family: Bold_Font; line-height: 18px;">Izabel Pacheco<br>
-                    <span style="font-family: Regular;">Fez cozinha e banheiro<br>
-                    com a <b style="font-family: Bold_Italic;">Casa Nobre Planejados<b></span></p>
-                </div>
-                <div class="col-md-1 col-md-offset-1" style="width: 15%; margin-left: 11%;">
-                    <img alt="image" class="pull-right img-cn img-circle" src="resources/img/test-1.jpg">
-                </div>
-                <div class="col-md-2 text-left pull-left">
-                    <p></p>
-                    <p style="font-family: Bold_Font; line-height: 18px;">Chaves<br>
-                    <span style="font-family: Regular;">Fez o baú<br>
-                    com a <b style="font-family: Bold_Italic;">Casa Nobre Planejados<b></span></p>
-                </div>
-            </div>
+
         </div>
     </section>
 
@@ -75,32 +82,21 @@
                 <div class="col-md-3 text-center">
                     <img class="text-center" src="resources/img/QualidadeNaEntrega.png" style="margin-bottom: 15px !important;">
                     <p style="color: #C24F56; font-size: 25px; font-family: Semibold_Italic !important;">Qualidade na<br>Entrega</p>
+                    <p style="font-family: Semibold_Italic !important; font-size: 14px;">Trabalhamos com<br> materiais selecionados e <br>profissionais treinados.</p>
                 </div>
                 <div class="col-md-3 text-center">
                     <img class="text-center" src="resources/img/AtendimentoDiferenciado.png" style="margin-bottom: 15px !important;">
                     <p style="color: #C24F56; font-size: 25px; font-family: Semibold_Italic !important;">Atendimento<br>Diferenciado</p>
+                    <p style="font-family: Semibold_Italic !important;">Honestidade e respeito<br> é a nossa base.</p>
                 </div>
                 <div class="col-md-3 text-center">
                     <img class="text-center" src="resources/img/FacilidadeNoPagamento.png" style="margin-bottom: 15px !important;">
                     <p style="color: #C24F56; font-size: 25px; font-family: Semibold_Italic !important;">Facilidade no<br>Pagamento</p>
+                    <p style="font-family: Semibold_Italic !important;">Parcele até 24 vezes.<br> Orçamento a um preço<br> justo.</p>
                 </div>
                 <div class="col-md-3 text-center">
                     <img class="text-center" src="resources/img/3AnosDeGarantia.png" style="margin-bottom: 15px !important;">
                     <p style="color: #C24F56; font-size: 25px; font-family: Semibold_Italic !important;">3 anos de<br>garantia</p>
-                </div>                                
-            </div>
-
-            <div class="row">
-                <div class="col-md-3 text-center">
-                    <p style="font-family: Semibold_Italic !important; font-size: 14px;">Trabalhamos com<br> materiais selecionados e <br>profissionais treinados.</p>
-                </div>
-                <div class="col-md-3 text-center">
-                    <p style="font-family: Semibold_Italic !important;">Honestidade e respeito<br> é a nossa base.</p>
-                </div>
-                <div class="col-md-3 text-center">
-                    <p style="font-family: Semibold_Italic !important;">Parcele até 24 vezes.<br> Orçamento a um preço<br> justo.</p>
-                </div>
-                <div class="col-md-3 text-center">
                     <p style="font-family: Semibold_Italic !important;">Sua satisfação é nossa<br> prioridade!</p>
                 </div>                                
             </div>
@@ -117,9 +113,7 @@
         <section style="margin-top: 0px !important; margin-bottom: 0px !important; padding-top: 40px !important; padding-bottom: 0px !important;">
             <div class="container-fluid" style="margin-bottom: 2px border-color: #C24F56" >
                 <div class="row">
-                    <div id="map_google" class="col-md-6 text-right select-home" style=" height:50px; margin-left: 0px !important; width:  !important; "><p id="p_map_google" style="padding-top: 13px !important; font-size: 18px !important; color: #fff">Localização</p></div>
-
-                    <div id="fotos_carousel"  class="col-md-6 text-left deselect-home" style="height:50px; width: 50% !important;"><p id="p_fotos_carousel" style="padding-top: 15px !important; font-size: 14px !important;"><!-- Fotos do Instagram --></p></div>
+                    <div id="map_google" class="text-center select-home" style=" height:50px; margin-left: 0px !important; width:  !important; "><p id="p_map_google" style="padding-top: 13px !important; font-size: 18px !important; color: #fff">Localização</p></div>
                 </div>
                 <div  id="line_map" class="row" style="margin-bottom: -6px !important; padding-bottom: 0px !important;">
                         <iframe  style="margin-bottom: 0px !important; padding-bottom: 0px !important;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3660.03901426177!2d-46.55042564855444!3d-23.4590571636344!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cef50cf7473765%3A0x94614946c2e08760!2sAv.+Torres+Tibagy%2C+870+-+Vila+Aprazivel%2C+Guarulhos+-+SP!5e0!3m2!1spt-BR!2sbr!4v1508985729569" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
@@ -157,7 +151,7 @@
                     </div> -->
                 </div>                
                 <div class="row">
-                    <div class="col-md-12" style="background-color: #C24F56 !important; height:3px; margin-left: 0px !important; width:  !important;"></div>
+                    <div style="background-color: #C24F56 !important; height:3px; margin-left: 0px !important; width:  !important;"></div>
                 </div>                
             </div>
         </section>
