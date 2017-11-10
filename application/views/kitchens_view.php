3aa9eaeefe8686b304de34d5c80e849c9f6d4942
Gabriel Faganello <faganello_@hotmail.com>
2017-11-10 02:16:29 -0200

    adicinado caminho da imagens top e bot para apasta original

--- application/views/kitchens_view.php
+++ application/views/kitchens_view.php
@@ -1,6 +1,6 @@
 
     <section class="main-home home-size" id="home">
-      <div class="kitchens-top-bg default-bg" style="position: static;"></div>
+      <div class="kitchens-top-bg default-bg"></div>
       <div class="home__header-content">
       <div class="col-md-offset-1 col-md-11">
         <p class="intro-ambiente-cn text-left" style="font-family: Semibold !important; letter-spacing: 4px !important; font-size: 36px;">[ Cozinha ]</p>
