71434b59e2f2f132f78adb14b350517960a29f83
Gabriel Faganello <faganello_@hotmail.com>
2017-11-11 02:02:49 -0200

    ajuste alinhamento do texto visit

--- application/views/visit_view.php
+++ application/views/visit_view.php
@@ -1,22 +1,24 @@
 <section id="visit" style="margin-top: 10px !important; margin-bottom: 10px !important; padding-bottom: 10px !important; padding-top: 10px !important;">
     <div class="container">
         
-    <div class="row title-sub-cn text-center" style="margin-bottom: 50px !important;">
+    <div class="row title-sub-cn text-center visit-title-margin">
             <span class="font-weight" style="font-weight: lighter; !important">[</span>
             <span class="title-sub-cn" style="margin-top: 40px; font-weight: lighter !important"">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Venhas no visitar!&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
             <span class="font-weight" style="font-weight: lighter !important">]</span>
     </div>
 
-        <div class="row text-center">
-                <p style="font-family: Bold_Font; font-size: 18px;">Atualmente trabalhamos nas regiões de São Paulo e Guarulhos.</p>
-                <p style="font-family: Bold_Italic; font-size: 20px; letter-spacing:1px;">Passe em um de nossos show rooms para nos conhecer, sem compromisso!</p><br>
+    <div class="row text-center">
+        <div class="col-md-12">
+            <p class="visit-p-margin">Atualmente trabalhamos nas regiões de São Paulo e Guarulhos.</p>
+            <p style="font-family: Bold_Italic; font-size: 20px; letter-spacing:1px;">Passe em um de nossos show rooms para nos conhecer, sem compromisso!</p><br>
         </div>
+    </div>
 
-        <div class="row">
-            <div class="col-md-12 text-center">
-                <a href="#" class="btn btn-custom-outline-red" style="font-family: Bold_Font; font-size: 18px; letter-spacing:1px;">ENTRE EM CONTATO</a>
-            </div>
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <a href="#" class="btn btn-custom-outline-red" style="font-family: Bold_Font; font-size: 18px; letter-spacing:1px;">ENTRE EM CONTATO</a>
         </div>
+    </div>
 
     </div>
 </section>
