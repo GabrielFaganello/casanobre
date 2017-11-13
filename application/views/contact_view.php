0994aeabfc0fd9f7cd0bae964c2c956b7f27093c
Tiago Neves <tiago.neves96@gmail.com>
2017-11-13 00:58:56 -0200

    contact

--- application/views/contact_view.php
+++ application/views/contact_view.php
@@ -10,8 +10,60 @@
     <section >
         <div class="row">
             <div class="col-md-12">
-            
-                <form class=" text-center" method="POST" action='<?=base_url('enviar');?>'>
+                <span style="font-weight:bold; margin-top: 20px; font-family: Regular; font-size: 18px;">Telefone:</span>
+                <br>
+                <span style="font-weight: bold !important; color: #C24F56 !important; font-size: 20px !important; padding: 0px !important; padding: 0px !important; margin: 0px !important;" >(11)4574-4443</span>
+                <br><br><br>
+                <span style="font-weight:bold; margin-top: 20px; font-family: Regular; font-size: 18px;">Nossas Redes:</span>
+                <br>
+                <div class="row col col-md-1">
+                    <img src="resources/img/facebook.png" style="margin-bottom: 15px; float:left">
+                    <span style="font-weight:bold; margin-top: 20px; font-family: Regular; font-size: 18px;">@casanobreplanejados</span>
+                    
+                </div>
+
+            </div>
+
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <form class=" text-center" method="POST" action='<?=base_url('enviar');?>'> 
+                    <div class="col-md-offset-5 col-md-2">
+                        <label style="float: left;" for="nome">Nome:</label>
+                        <span style="float: right;"><input type="text" id="nome" placeholder="Nome" required autofocus></span>
+                        <br><br>
+                    </div>
+                    <div class="col-md-offset-5 col-md-2">
+                         <label style="float: left;" for="fone">Telefone:</label>
+                         <span style="float: right;"><input type="text" id="fone" placeholder="Telefone/Celular" required data-mask="(00) 00000-0000"></span>
+                         <br><br>
+                    </div>
+                    <div class="col-md-offset-5 col-md-2">
+                         <label style="float:left;" for="assunto">Assunto:</label>
+                         <span style="float:right;"><input type="text" id="assunto" placeholder="Assunto" required></span>
+                         <br><br>
+                    </div>
+                    <div class="col-md-offset-5 col-md-2" method="POST" action='<?=base_url('enviar');?>'>
+              
+                         <label style="float: left;" for="textDe" id="from">E-mail:</label></span>
+                         <span style="float: right;"><input id="textDe" type="text" name="txt_de"/></span>
+                         <br><br>
+              
+                         <label style="float: left;" for="emailPara" id="destiny">E-mail de Destino:</label>
+                         <span style="float: right;"><input id="emailPara" type="text" name="txt_para"/></span>
+                         <br><br>
+                         
+                         <label style="float: left;" for="textMsg" id="text">Mensagem:</label><br>
+                         <span style="float: right;"><textarea name="emailMensagem" id="textMsg" rows=""></textarea></span>
+                         <br><br>
+             
+                         <input id="ButtonEnviar" type="submit" name="env" value="Enviar E-mail"/>
+                       <br><br>
+             
+                    </div>
+                </form>
+
+                    <!-- <form class=" text-center" method="POST" action='<?=base_url('enviar');?>'> 
 
                     <span id="destiny">E-mail de Destino:</span>
                     <input id="emailPara" type="text" name="txt_para"/>
@@ -25,7 +77,7 @@
          
                     <input id="ButtonEnviar" type="submit" name="env" value="Enviar E-mail"/>
          
-                </form>
+                </form>-->
             </div>
         </div>
     </section>
