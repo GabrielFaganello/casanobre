2d09685bdd1e1a1fee496559330db4f7f1391d1d
Gabriel Faganello <faganello_@hotmail.com>
2017-11-27 11:40:07 -0200

    ajustes

--- application/models/casanobre_model.php
+++ application/models/casanobre_model.php
@@ -2,17 +2,17 @@
 
 class casanobre_model extends CI_Model{
 
-   public final function __construct()
-   {
-      parent::__construct();
-   }
+	public final function __construct()
+	{
+	  parent::__construct();
+	}
 
-    function enviar_email(){
+	function enviar_email(){
 
-		$nome_contato = $this->input->post('nome');    //CAPTURA O VALOR DA CAIXA DE TEXTO 'E-mail de Destino'
-		$fone_contato = $this->input->post('fone');    //CAPTURA O VALOR DA CAIXA DE TEXTO 'E-mail de Destino'
-		$assunto_contato = $this->input->post('assunto');    //CAPTURA O VALOR DA CAIXA DE TEXTO 'E-mail de Destino'
-		$email_contato = $this->input->post('email');    //CAPTURA O VALOR DA CAIXA DE TEXTO 'E-mail de Destino'
+		$nome_contato = $this->input->post('nome');
+		$fone_contato = $this->input->post('fone');
+		$assunto_contato = $this->input->post('assunto');
+		$email_contato = $this->input->post('email');
 		$msg_contato = $this->input->post('emailMensagem');
 
 		$mensagem = "Nome: $nome_contato\n".
@@ -22,22 +22,16 @@ class casanobre_model extends CI_Model{
 					"Mensagem: $msg_contato";
 
 
-		$this->email->from('casanobreplanejados@casanobreplanejados.com.br', 'Contato Planejados');                //ESPECIFICA O FROM(REMETENTE) DA MENSAGEM DENTRO DA CLASSE
-		$this->email->to('casanobreplanejados@casanobreplanejados.com.br');                         //ESPECIFICA O DESTINATÁRIO DA MENSAGEM DENTRO DA CLASSE  
-		$this->email->subject('Contato');         //ESPECIFICA O ASSUNTO DA MENSAGEM DENTRO DA CLASSE
-		$this->email->message($mensagem);	                 //ESPECIFICA O TEXTO DA MENSAGEM DENTRO DA CLASSE
+		$this->email->from('casanobreplanejados@casanobreplanejados.com.br', 'Contato Planejados');
+		$this->email->to('casanobreplanejados@casanobreplanejados.com.br');
+		$this->email->subject('Contato');
+		$this->email->message($mensagem);
 		
 		if ($this->email->send()) {
-			// $this->email->send();    
 			$retorno = TRUE;
 		}else{
-
 			$retorno = FALSE;
 		}
-		
-		// $debug_msg =  $this->email->print_debugger();
-// 
-		// var_dump($debug_msg , $retorno);die;             //COMANDO QUE MOSTRA COMO ACONTECEU O ENVIO DA MENSAGEM NO SERVIDOR
 
 		return $retorno;
 	}
